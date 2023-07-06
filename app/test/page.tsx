e286f36fce7bc2ff2e6e822fe003a2e7e84422bb
Andrew <andrew@andrewsmith.org>
2023-07-06 02:03:19 -0400

    Satisfy Prettier

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,27 +1,30 @@
 export default function Test() {
-    const apiCall = async () => {
-        try {
-            const response = await fetch("https://www.bungie.net/Platform/Destiny2/Manifest/", {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-API-Key": `${process.env.REACT_APP_DESTINY_API_KEY}`,
-                }
-            })
-            const data = await response.json()
-            return data.Response.version
-        } catch(err) {
-            console.warn(err)
+  const apiCall = async () => {
+    try {
+      const response = await fetch(
+        'https://www.bungie.net/Platform/Destiny2/Manifest/',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': `${process.env.REACT_APP_DESTINY_API_KEY}`,
+          },
         }
+      );
+      const data = await response.json();
+      console.log(data);
+      return data.Response.version;
+    } catch (err) {
+      console.warn(err);
     }
+  };
 
-    const apiData = apiCall()
+  const apiData = apiCall();
 
-
-    return (
-        <div>
-            <p>Hello world</p>
-            <p>{apiData}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>Hello world</p>
+      <p>{apiData}</p>
+    </div>
+  );
 }
