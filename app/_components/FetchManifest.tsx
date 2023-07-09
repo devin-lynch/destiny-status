8b9fc15d5cf76f4cae54d9aa6525e8ca3ea9dd56
Andrew <andrew@andrewsmith.org>
2023-07-08 20:28:57 -0400

    Refactor fetch logic into multiple useEffects

--- app/_components/FetchManifest.tsx
+++ app/_components/FetchManifest.tsx
@@ -1,9 +1,12 @@
 'use client';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { get, set } from 'idb-keyval';
 
 export default function FetchManifest() {
-  // get the most recent manifest version from bungie
+  const [fetchedVersionNumber, setFetchedVersionNumber] = useState('');
+  const [manifestPath, setManifestPath] = useState('');
+
+  // get the most recent manifest version and the location to download it
   const fetchManifestData = async () => {
     const response = await fetch(
       'https://www.bungie.net/Platform/Destiny2/manifest/'
@@ -18,37 +21,42 @@ export default function FetchManifest() {
     return cacheManifestVersion == fetchedVersionNumber;
   };
 
-  // if mismatch, fetch current manifest from bungie and save it in store
+  // fetch current manifest from bungie and save it in store
   const fetchManifest = async () => {
-    // would prefer to have saved the path in state and not have to invoke fetchManifestData here as it will have already been called once before this function is invoked.  was having some issues getting the promises to resolve in time though; will revisit and fix that later
-    const [fetchedVersionNumber, fetchedManifestPath] =
-      await fetchManifestData();
-    const response = await fetch(
-      `https://www.bungie.net${fetchedManifestPath}`
-    );
+    const response = await fetch(`https://www.bungie.net${manifestPath}`);
     const data = await response.json();
     console.log(data);
     // currently only saving one table from the manifest in local store as a proof of concept -- full manifest is larger than firefox will accept (works in Chrome apparently).  looks like DIM only stores the tables that are needed for app functionality, so I'll need to figure out a way to only save the chunks of the manifest that will actually be useful
     set('manifest', data.DestinyActivityDefinition);
   };
 
+  // initial API request to add relevant metadata to state
   useEffect(() => {
     (async () => {
-      const [fetchedVersionNumber, fetchedManifestPath] =
-        await fetchManifestData();
-      const manifestVersionsMatch = await compareManifestVersion(
-        fetchedVersionNumber
-      );
-      if (manifestVersionsMatch) {
-        console.log("it's a match");
-        await fetchManifest();
-      } else {
-        console.log('no match, fixing that');
-        set('cacheManifestVersion', fetchedVersionNumber);
-        await fetchManifest();
-      }
+      const [version, path] = await fetchManifestData();
+      setFetchedVersionNumber(version);
+      setManifestPath(path);
     })();
   }, []);
+
+  // once the previous effect is complete and state variables have been updated, check for version match and fetch the manifest if mismatch
+  useEffect(() => {
+    if (manifestPath !== '') {
+      (async () => {
+        const manifestVersionsMatch = await compareManifestVersion(
+          fetchedVersionNumber
+        );
+        if (manifestVersionsMatch) {
+          console.log("it's a match");
+          await fetchManifest();
+        } else {
+          console.log('no match, fixing that');
+          set('cacheManifestVersion', fetchedVersionNumber);
+          await fetchManifest();
+        }
+      })();
+    }
+  }, [manifestPath]);
 
   return (
     <>
