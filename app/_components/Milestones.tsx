f032d25ae4e844fa41206fddb0121b1db1855e8c
Andrew <andrew@andrewsmith.org>
2023-07-15 19:21:11 -0400

    Refactor to include multiple manifest tables in local store
    This commit also refactors some of the data organization; after adding the inventory item table to the datastore, the retrieval each component was doing was an obnoxiously long transaction.  Moved that to Components.tsx so we could do it once and then just pass the definitions down as props.

--- app/_components/Milestones.tsx
+++ app/_components/Milestones.tsx
@@ -1,22 +1,46 @@
 'use client';
 import { useManifestStatus } from '../_hooks/useManifestStatus';
+import { useState, useEffect } from 'react';
+import { get } from 'idb-keyval';
 import Milestone from './Milestone';
 
 export default function Milestones({ hashes }) {
+  const [milestoneDefinitions, setMilestoneDefinitions] = useState();
+  const [milestoneComponents, setMilestoneComponents] = useState([]);
   const manifestIsLoaded = useManifestStatus();
 
-  const hashComponents = hashes.map((hash, i: number) => {
-    return (
-      <li key={i}>
-        <Milestone hash={hash} />
-      </li>
-    );
-  });
+  const getMilestoneDefinitions = async () => {
+    const manifest = await get('manifest');
+    return manifest.DestinyMilestoneDefinition;
+  };
+
+  useEffect(() => {
+    (async () => {
+      const milestoneDefinitions = await getMilestoneDefinitions();
+      setMilestoneDefinitions(milestoneDefinitions);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (manifestIsLoaded) {
+      const milestoneComponents: JSX.Element[] = hashes.map(
+        (hash, i: number) => {
+          return (
+            <li key={i}>
+              <p>hi</p>
+              <Milestone milestoneDefinition={milestoneDefinitions[hash]} />
+            </li>
+          );
+        }
+      );
+      setMilestoneComponents(milestoneComponents);
+    }
+  }, [milestoneDefinitions]);
 
   return (
     <div>
       {manifestIsLoaded ? (
-        hashComponents
+        milestoneComponents
       ) : (
         <p>Retrieving manifest data from Bungie...</p>
       )}
