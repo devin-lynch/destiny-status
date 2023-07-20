8c5446324dbd383981db63eeb10d192df3077c63
Andrew <andrew@andrewsmith.org>
2023-07-20 17:25:07 -0400

    Refactor to include debouncing

--- app/apitest/page.tsx
+++ app/apitest/page.tsx
@@ -1,25 +1,27 @@
 "use client";
-import { ChangeEvent, useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function apitest() {
   const [username, setUsername] = useState("");
 
-  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    try {
-      setUsername(e.target.value);
-      const response = await fetch(
-        "http://localhost:3000/api/serverless-example",
-        {
-          method: "POST",
-          body: JSON.stringify({ username: e.target.value }),
-        }
-      );
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  useEffect(() => {
+    const fetchUsers = setTimeout(async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/api/serverless-example",
+          {
+            method: "POST",
+            body: JSON.stringify({ username }),
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }, 300);
+    return () => clearTimeout(fetchUsers);
+  }, [username]);
 
   return (
     <div className="flex justify-center mt-2">
@@ -27,7 +29,7 @@ export default function apitest() {
         value={username}
         placeholder="search by Bungie name..."
         className="bg-slate-900 border border-slate-500"
-        onChange={(e) => handleChange(e)}
+        onChange={(e) => setUsername(e.target.value)}
       ></input>
     </div>
   );
