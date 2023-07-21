ae561c043df916e59dcaa795d69330910185e7ff
devin-lynch <devin.l.lynch@gmail.com>
2023-07-21 16:06:17 -0500

    Remove milestone content

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,6 @@
-import Milestones from './_components/Milestones';
 import CharacterContainer from './_components/CharacterContainer';
 
 export default async function Home() {
-  const fetchMilestones = async () => {
-    const response = await fetch(
-      'https://www.bungie.net/Platform/Destiny2/Milestones/',
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-API-Key': `${process.env.REACT_APP_DESTINY_API_KEY}`,
-        },
-      }
-    );
-    const data = await response.json();
-    return data.Response;
-  };
-
-  const apiMilestoneResponse = await fetchMilestones();
-
-  const currentMilestoneHashes = Object.keys(apiMilestoneResponse).map(
-    (key) => {
-      return parseInt(key);
-    }
-  );
-
   const fetchCharacters = async () => {
     const response = await fetch(
       'http://localhost:3000/api/get-bungie-profile',
@@ -50,7 +26,6 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center pt-24">
       <p>hi 👋</p>
       <CharacterContainer characterData={characterData} />
-      <Milestones hashes={currentMilestoneHashes} />
     </main>
   );
 }
