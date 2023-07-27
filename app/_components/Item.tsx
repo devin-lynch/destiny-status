93a18a673a7825f9ed7ee26f67e24bfff05ab38a
Andrew <andrew@andrewsmith.org>
2023-07-27 18:46:45 -0400

    Remove nested main element

--- app/_components/Item.tsx
+++ app/_components/Item.tsx
@@ -12,18 +12,16 @@ type Props = {
 
 export default function Item({ item }: Props) {
   return (
-    <main>
-      <div className="text-center mt-8">
-        <p className="text-xl">{item.displayProperties.name}</p>
-        <div className="flex justify-center">
-          <img
-            src={`https://bungie.net${item.displayProperties.icon}`}
-            alt=""
-            style={{ height: '8em' }}
-          />
-        </div>
-        <p className="text-sm italic mt-2">{item.itemTypeAndTierDisplayName}</p>
+    <div className="text-center mt-8">
+      <p className="text-xl">{item.displayProperties.name}</p>
+      <div className="flex justify-center">
+        <img
+          src={`https://bungie.net${item.displayProperties.icon}`}
+          alt=""
+          style={{ height: '8em' }}
+        />
       </div>
-    </main>
+      <p className="text-sm italic mt-2">{item.itemTypeAndTierDisplayName}</p>
+    </div>
   );
 }
